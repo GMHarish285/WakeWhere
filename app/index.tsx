b0684fee1f5d9df0363e6b3bfc6d75667efac393
Harish G M <gmharish285@gmail.com>
2026-03-05 00:09:31 +0530

    style: improve tracking status card layout

--- app/index.tsx
+++ app/index.tsx
@@ -4,16 +4,17 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1">
+    <View className="flex-1 p-4 bg-gray-200 gap-4">
       <View className="items-end">
         <Text>Settings</Text>
         <Text>+</Text>
       </View>
+
       <View>
-        <Text>Tracking Status</Text>
         {/* <Text>Not tracking any location</Text> */}
         <TrackingStatus />
       </View>
+      
       <View>
         <Text>Saved Configs</Text>
         {/* <Text>No saved configs yet</Text> */}
--- components/TrackingStatus.tsx
+++ components/TrackingStatus.tsx
@@ -1,24 +1,51 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, View } from "react-native";
 
-export function TrackingStatus() {
+export function TrackingStatus({
+  isTracking = true,
+  configName = "Config Name",
+  lat = "0.0000",
+  lon = "0.0000",
+  thres = "500",
+  dist = "1.5",
+}) {
   return (
-    <View>
-      <View>
-        <Text>Config name</Text>
-      </View>
-      <View className="flex-row">
-        <Text>Latitude</Text>
-        <Text>Longitude</Text>
-      </View>
-      <View>
-        <Text>Threshold</Text>
-      </View>
-      <View>
-        <Text>Distance</Text>
-      </View>
-      <View className="">
-        <Text>Stop tracking</Text>
-      </View>
+    <View className="border bg-white rounded-xl p-4 gap-3 items-center">
+      <Text className="text-lg mb-3 font-bold">Tracking Status</Text>
+
+      {isTracking ? (
+        <>
+          <View>
+            <Text className="text-base font-semibold">{configName}</Text>
+          </View>
+
+          <View className="flex-row justify-between gap-8">
+            <View className="flex-row gap-2">
+              <Text className="text-gray-900">Lat</Text>
+              <Text className="font-medium">{lat}</Text>
+            </View>
+            <View className="flex-row gap-2">
+              <Text className="text-gray-900">Lon</Text>
+              <Text className="font-medium">{lon}</Text>
+            </View>
+          </View>
+
+          <View className="flex-row gap-4">
+            <Text className="text-gray-900">Threshold</Text>
+            <Text className="font-medium">{thres} m</Text>
+          </View>
+
+          <View className="flex-row gap-4">
+            <Text className="text-gray-900">Distance</Text>
+            <Text className="font-medium">{dist} km</Text>
+          </View>
+
+          <Pressable className="mt-2 bg-red-500 py-2 px-8 rounded-lg items-center">
+            <Text className="text-white">Stop tracking</Text>
+          </Pressable>
+        </>
+      ) : (
+        <Text className="text-gray-500">Not tracking any location</Text>
+      )}
     </View>
   );
 }
